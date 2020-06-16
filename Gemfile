1bf2d1679f4b33b562fce24c175c292d5a30b053
ryosuke-ichikawa03 <ickwrsk@gmail.com>
2020-06-16 16:32:33 +0900

    index_top img

--- Gemfile
+++ Gemfile
@@ -68,3 +68,4 @@ gem 'gretel'
 gem 'carrierwave'
 gem 'mini_magick'
 gem 'jquery-rails'
+
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,19 @@
 class CategoriesController < ApplicationController
   def index
     @products = Product.includes(:images).order("created_at DESC")
+    @images = Image.order(id: "DESC")
   end
+
+  # def index_category_set
+  #   array = [1,2,3,4,]
+  #     for num in array do
+  #       search_anc = Category.where('ancentry LIKE(?)',"#{num}/%")
+  #       ids = []
+  #       serch_anc.each do |i|
+  #         ids << i[:id]
+  #       end
+  #       products = Product.where(category_id: ids).oder("id DESC")
+  #       instance_variable_set("@cat_no#{num}",products)
+  #     end
+  # end
 end
