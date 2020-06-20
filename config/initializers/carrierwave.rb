164374d457f3e3e56e335f0c2f9510f581fc76d6
salt00san <missie.sol09@gmail.com>
2020-06-20 23:41:57 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,14 +6,14 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage = :fog
     config.fog_provider = "fog/aws"
-    config.fog_directory = "fleamarket72b"
-    config.asset_host = "https://s3-ap-northeast-1.amazonaws.com/fleamarket72b"
     config.fog_credentials = {
       provider: "AWS",
       aws_access_key_id: Rails.application.credentials[:aws][:access_key_id],
       aws_secret_access_key: Rails.application.credentials[:aws][:secret_access_key],
       region: "ap-northeast-1",
     }
+    config.fog_directory = "fleamarket72b"
+    config.asset_host = "https://fleamarket72b.s3.amazonaws.com"
   else
     config.storage :file
     config.enable_processing = false if Rails.env.test?
