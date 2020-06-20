29a2bb6123bba88a3017a6590cf93829cf907a60
Shinoda492039 <a99yf47.12x@gmail.com>
2020-06-20 20:31:41 +0900

    Update searches_controller.rb

--- app/controllers/products/searches_controller.rb
+++ app/controllers/products/searches_controller.rb
@@ -1,5 +1,5 @@
 class Products::SearchesController < ApplicationController
   def index
-    @products = Product.search(params[:keyword])
+    @products = Product.search(params[:keyword]).order("created_at DESC")
   end
 end
