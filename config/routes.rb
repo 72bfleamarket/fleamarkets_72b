97433f601fecc739babbadacee7c5a651b78dcbd
net-pixel <masataka.for.ofua@gmail.com>
2020-06-21 21:32:02 +0900

    FinalCommit

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
     get  'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
+  devise_scope :user do
+    get  'password', to: 'users/passwords#new'
+    # post 'user', to: 'users//passwords#create'
+  end
   resources :products, only: [:index, :new, :create, :show] do
     resources :categories, only:[:create]
   end
