7c6107c02ba42eba70596dada2e66024a1ae1761
net-pixel <masataka.for.ofua@gmail.com>
2020-06-21 22:55:46 +0900

    コメントアウト

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  # before_action :configure_sign_up_params, only: [:create]　/デフォルト記載
+  # before_action :configure_account_update_params, only: [:update]　/デフォルト記載
 
-  # GET /resource/sign_up
   def new_user
     @user = User.new
   end
 
-  # POST /resource
   def create_user
     params[:user][:birthday] = birthday_join
     @user = User.new(sign_up_params)
@@ -49,13 +47,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
     Date.new(@date.values[0].to_i, @date.values[1].to_i, @date.values[2].to_i)
   end
 
-  # If you have extra params to permit, append them to the sanitizer.
+  # If you have extra params to permit, append them to the sanitizer. /デフォルト記載
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :password, :email, :first_name,:last_name, :first_kana, :last_kana, :birthday])
   end
 
 
-  # def sign_up_params
+  # def sign_up_params　/デフォルト記載
   #   params.require(:user).permit(:name, :email, :first_name,:last_name, :first_kana, :last_kana, :birthday)
   # end
 
