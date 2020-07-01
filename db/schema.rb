ea9f55eff58461a91996cd9fafcedace4ed86370
Shinoda492039 <a99yf47.12x@gmail.com>
2020-07-01 17:55:05 +0900

    cardsテーブルのカラムを更新
    参考URLの手順通りに各種ファイルを更新し、カードの登録・削除可否を検証するため。
    
    参考URL：
    https://qiita.com/takachan_coding/items/f7e70794b9ca03b559dd#実装する機能と前提条件

--- db/schema.rb
+++ db/schema.rb
@@ -28,11 +28,9 @@ ActiveRecord::Schema.define(version: 2020_06_20_095121) do
   end
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "number"
-    t.date "date"
-    t.string "name"
-    t.integer "security"
     t.integer "user_id"
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_cards_on_user_id"
