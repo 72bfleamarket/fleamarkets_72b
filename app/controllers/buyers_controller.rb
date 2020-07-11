b3c966979f074798340ae5dfd7e3877df682441f
Shinoda492039 <a99yf47.12x@gmail.com>
2020-07-11 12:46:41 +0900

    クレジットカード未登録状態で商品を購入しようとした際、アラートを表示
    また、カードサイズの指定を_cards.scssへ移動

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -8,7 +8,7 @@ class BuyersController < ApplicationController
     @buyer_last_name = current_user.last_name
 
     if @card.present?
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_PRIVATE_KEY]
       #保管した顧客IDでpayjpから情報取得
       customer = Payjp::Customer.retrieve(@card.customer_id) 
       #カード情報表示のためインスタンス変数に代入
@@ -17,7 +17,7 @@ class BuyersController < ApplicationController
       @exp_year = @default_card_information.exp_year.to_s.slice(2,3)
     else
       #登録された情報がない場合にカード登録画面に移動
-      redirect_to new_card_path
+      redirect_to new_card_path, notice: '商品を購入するには、クレジットカードの登録が必要です'
     end
   end
 
