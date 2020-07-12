aed16153190dba49017fc477558a2cd4db5870f1
Shinoda492039 <a99yf47.12x@gmail.com>
2020-07-12 18:03:52 +0900

    ユーザーマイページのパンくずリストを追加

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,7 +1,9 @@
+# 共通で表示させたいパンくずリスト
 crumb :root do
   link "FURIMA", root_path
 end
 
+# 商品関連のパンくずリスト
 crumb :categories do
   link "すべてのカテゴリー", categories_path
   parent :root
@@ -26,6 +28,18 @@ crumb :product do
   link Product.find(params[:id]).name
   parent :grandchildren
 end
+
+#ユーザーマイページのパンくずリスト
+crumb :mypage do
+  link "マイページ", user_path(current_user.id)
+  parent :root
+end
+
+crumb :payment do
+  link "支払い方法", new_card_path
+  parent :mypage
+end
+
 # If you want to split your breadcrumbs configuration over multiple files, you
 # can create a folder named `config/breadcrumbs` and put your configuration
 # files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
