09160abb381d183dbc29b5ab016707fcce80083b
Shinoda492039 <a99yf47.12x@gmail.com>
2020-07-15 00:52:26 +0900

    検索結果画面にサイドバーを追加し、サイドバーからも検索する機能を追加
    その他、画面のレイアウトも更新

--- app/controllers/products/searches_controller.rb
+++ app/controllers/products/searches_controller.rb
@@ -1,5 +1,6 @@
 class Products::SearchesController < ApplicationController
   def index
-    @products = Product.search(params[:keyword]).order("created_at DESC")
+    @search_word = params[:keyword]
+    @products = Product.search(@search_word).order("created_at DESC")
   end
 end
