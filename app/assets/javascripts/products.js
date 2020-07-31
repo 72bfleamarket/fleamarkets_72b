700ed6c6a648a4fd9b8ecfd987c7f82eddedb94f
net-pixel <masataka.for.ofua@gmail.com>
2020-07-31 23:44:20 +0900

    不要なJS削除

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -10,11 +10,8 @@ $(window).load(function () {
       return field;
     }
 
-    let fileIndex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
     //画像ファイルプレビュー表示のイベント追加 fileを選択時に発火するイベントを登録
-    $('.box').on('change', 'input[type="file"]', function (e) {
-      const targetIndex = $(this).parent().data('index');
+    $('.box').on('change', 'input[type="file"]', function(e) {
       const file = e.target.files[0];
       reader = new FileReader();
       let box = $(".box");
@@ -27,7 +24,7 @@ $(window).load(function () {
       }
       // ファイル読み込みが完了した際のイベント登録
       reader.onload = (function (file) {
-          return function (e) {
+          return function () {
             let src = reader.result
             let html = `<div class='preview' data-image="${file.name}">
           <div class='preview__content'>
@@ -38,96 +35,53 @@ $(window).load(function () {
 
             //box要素の前にhtmlを差し込む
             $(".dummy").before(html);
-            let num = fileIndex.shift();
             box.append(buildFileField(dropbox_no));
 
 
             //写真追加によるviewの変更
             switch (dropbox_no) {
               case 10:
-                $(".field-10").css({
-                  'display': 'none'
-                })
-                $(".field-9").css({
-                  'display': 'none'
-                })
+                $(".field-10").css({'display': 'none'})
+                $(".field-9").css({'display': 'none'})
                 break;
               case 9:
-                $(".field-9").css({
-                  'width': '20%'
-                })
-                $(".field-8").css({
-                  'display': 'none'
-                })
+                $(".field-9").css({'width': '20%'})
+                $(".field-8").css({'display': 'none'})
                 break;
               case 8:
-                $(".field-8").css({
-                  'width': '40%'
-                })
-                $(".field-7").css({
-                  'display': 'none'
-                })
+                $(".field-8").css({'width': '40%'})
+                $(".field-7").css({'display': 'none'})
                 break;
               case 7:
-                $(".field-7").css({
-                  'width': '60%'
-                })
-                $(".field-6").css({
-                  'display': 'none'
-                })
+                $(".field-7").css({'width': '60%'})
+                $(".field-6").css({'display': 'none'})
                 break;
               case 6:
-                $(".field-6").css({
-                  'width': '80%'
-                })
-                $(".field-5").css({
-                  'display': 'none'
-                })
+                $(".field-6").css({'width': '80%'})
+                $(".field-5").css({'display': 'none'})
                 break;
               case 5:
-                $(".field-5").css({
-                  'width': '100%'
-                })
-                $(".field-4").css({
-                  'display': 'none'
-                })
+                $(".field-5").css({'width': '100%'})
+                $(".field-4").css({'display': 'none'})
                 break;
               case 4:
-                $(".field-4").css({
-                  'width': '20%'
-                })
-                $(".field-3").css({
-                  'display': 'none'
-                })
+                $(".field-4").css({'width': '20%'})
+                $(".field-3").css({'display': 'none'})
                 break;
               case 3:
-                $(".field-3").css({
-                  'width': '40%'
-                })
-                $(".field-2").css({
-                  'display': 'none'
-                })
+                $(".field-3").css({'width': '40%'})
+                $(".field-2").css({'display': 'none'})
                 break;
               case 2:
-                $(".field-2").css({
-                  'width': '60%'
-                })
-                $(".field-1").css({
-                  'display': 'none'
-                })
+                $(".field-2").css({'width': '60%'})
+                $(".field-1").css({'display': 'none'})
                 break;
               case 1:
-                $(".field-1").css({
-                  'width': '80%'
-                })
-                $(".field-0").css({
-                  'display': 'none'
-                })
+                $(".field-1").css({'width': '80%'})
+                $(".field-0").css({'display': 'none'})
                 break;
               case 0:
-                dropbox.css({
-                  'width': '100%'
-                })
+                dropbox.css({'width': '100%'})
                 break;
             }
 
@@ -144,84 +98,48 @@ $(window).load(function () {
               //写真追加によるviewの変更
               switch (images_no) {
                 case 9:
-                  $(".field-9").css({
-                    'width': '20%'
-                  })
-                  $(".field-9").css({
-                    'display': 'block'
-                  })
+                  $(".field-9").css({'width': '20%'})
+                  $(".field-9").css({'display': 'block'})
                   $(".field-10").remove()
                   break;
                 case 8:
-                  $(".field-8").css({
-                    'width': '40%'
-                  })
-                  $(".field-8").css({
-                    'display': 'block'
-                  })
+                  $(".field-8").css({'width': '40%'})
+                  $(".field-8").css({'display': 'block'})
                   $(".field-9").remove()
                   break;
                 case 7:
-                  $(".field-7").css({
-                    'width': '60%'
-                  })
-                  $(".field-7").css({
-                    'display': 'block'
-                  })
+                  $(".field-7").css({'width': '60%'})
+                  $(".field-7").css({'display': 'block'})
                   $(".field-8").remove()
                   break;
                 case 6:
-                  $(".field-6").css({
-                    'width': '80%'
-                  })
-                  $(".field-6").css({
-                    'display': 'block'
-                  })
+                  $(".field-6").css({'width': '80%'})
+                  $(".field-6").css({'display': 'block'})
                   $(".field-7").remove()
                   break;
                 case 5:
-                  $(".field-5").css({
-                    'width': '100%'
-                  })
-                  $(".field-5").css({
-                    'display': 'block'
-                  })
+                  $(".field-5").css({'width': '100%'})
+                  $(".field-5").css({'display': 'block'})
                   $(".field-6").remove()
                   break;
                 case 4:
-                  $(".field-4").css({
-                    'width': '20%'
-                  })
-                  $(".field-4").css({
-                    'display': 'block'
-                  })
+                  $(".field-4").css({'width': '20%'})
+                  $(".field-4").css({'display': 'block'})
                   $(".field-5").remove()
                   break;
                 case 3:
-                  $(".field-3").css({
-                    'width': '40%'
-                  })
-                  $(".field-3").css({
-                    'display': 'block'
-                  })
+                  $(".field-3").css({'width': '40%'})
+                  $(".field-3").css({'display': 'block'})
                   $(".field-4").remove()
                   break;
                 case 2:
-                  $(".field-2").css({
-                    'width': '60%'
-                  })
-                  $(".field-2").css({
-                    'display': 'block'
-                  })
+                  $(".field-2").css({'width': '60%'})
+                  $(".field-2").css({'display': 'block'})
                   $(".field-3").remove()
                   break;
                 case 1:
-                  $(".field-1").css({
-                    'width': '80%'
-                  })
-                  $(".field-1").css({
-                    'display': 'block'
-                  })
+                  $(".field-1").css({'width': '80%'})
+                  $(".field-1").css({'display': 'block'})
                   $(".field-2").remove()
                   break;
                 case 0:
@@ -256,12 +174,12 @@ $(window).load(function () {
   $('#count_name').trigger('input');
   $('#count_area').trigger('input');
 
-  //SHOWPAGE画像きり変え
-  $('.subImg').mouseover(function(e) {
-    const url = $(this).attr("src");
-    $('.mainImg').attr('src', url)
-  })
-  $('.subImg').on('click', function(){
-    $('.mainImg').mouseover()
-  })
+    //SHOWPAGE画像きり変え
+    $('.subImg').mouseover(function(e) {
+      const url = $(this).attr("src");
+      $('.mainImg').attr('src', url)
+    })
+    $('.subImg').on('click', function(){
+      $('.mainImg').mouseover()
+    })
 });
