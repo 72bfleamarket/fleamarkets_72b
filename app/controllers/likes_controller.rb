412b6f37c958b50ea1f7a81939b3e96821efd666
salt00san <missie.sol09@gmail.com>
2020-08-01 20:21:57 +0900

    current_userの修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,14 +3,12 @@ class LikesController < ApplicationController
   before_action :set_like
 
   def create
-    @user = current_user
-    @like = Like.create(user_id: @user.id, product_id: @product.id)
+    @like = Like.create(user_id: current_user.id, product_id: @product.id)
     @likes = Like.where(product_id: params[:product_id])
   end
 
   def destroy
-    @user = current_user
-    @like = Like.find_by(user_id: @user.id, product_id: @product.id)
+    @like = Like.find_by(user_id: current_user.id, product_id: @product.id)
     @like.destroy
     @likes = Like.where(product_id: params[:product_id])
   end
