104414dd6cc4e0911b2af4958bf0582f3b37d7f9
net-pixel <masataka.for.ofua@gmail.com>
2020-08-02 09:43:09 +0900

    CommentField

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(comment_params)
+    @comment = Comment.create(comment_params)
+    @comment = Comment.new(comment_params)
     redirect_to "/products/#{comment.product.id}"
   end
 
@@ -8,5 +9,5 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, product_id: params[:product_id])
   end
-  
+
 end
